1676443ce749fd9bc0b0560c73142c58ab6594d8
AlenOrbanic <aorbanic@student.unipu.hr>
2023-06-20 19:20:11 +0200

    izdrada jednostavnog middleware error handlera i ostalih backend ruta

--- src/routes/api.js
+++ src/routes/api.js
@@ -2,17 +2,47 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 
-// get list of users
-router.get('/user', function(req, res){
-res.send({type: 'GET'})
+// vraca listu svih usera
+router.get('/users', function(req, res, next) {
+  User.find({})
+    .then(function(users) {
+      res.send(users);
+    })
+    .catch(function(error) {
+      next(error);
+    });
 });
 
+
 // reg user
-router.post('/user', function(req, res) {
+router.post('/user', function(req, res, next) {
     User.create(req.body)
       .then(function(user) {
         res.send(user);
       })
+      .catch(next);
+  });
+  
+
+//update user (isto treba saveat cur user u store i onda zamjenit _id sa store.User.id ili store.user.email)
+router.put('/user/:id', function(req, res, next){
+    User.findByIdAndUpdate({_id: req.params.id}, req.body).then(function(){
+      User.findOne({_id: req.params.id}).then(function(user){
+        res.send(user)
+      });
+    });
+});
+
+//saveat current usera u store i zamjenit _id sa store.idem kojeg dobijemo iz monga ili samo izbrisat usera po mailu.
+router.delete('/user/:id', function(req, res, next){
+    User.findByIdAndRemove({_id: req.params.id}).then(function(user){
+      res.send(user);
+    });
+});
+
+module.exports = router;
+
+/*  Duplicate entry error handler
       .catch(function(error) {
         if (error.code === 11000 && error.keyPattern && error.keyValue) {
           // Duplicate key error occurred
@@ -25,16 +55,4 @@ router.post('/user', function(req, res) {
           res.status(500).send({ error: 'An error occurred' });
         }
       });
-  });
-  
-
-//update user?
-router.put('/user/:id', function(req, res){
-    res.send({type: 'PUT'})
-});
-
-router.delete('/user/:id', function(req, res){
-    res.send({type: 'DELETE'})
-});
-
-module.exports = router;
+*/
--- src/server.js
+++ src/server.js
@@ -8,6 +8,12 @@ app.use(bodyParser.json());
 // inicijalizacija ruta
 app.use('/api',require('./routes/api'));
 
+//error handling
+app.use(function(err, req, res, next){
+  //console.log(err);
+  res.status(422).send({error: err.message});
+});
+
 app.listen(8009, () =>{
     console.log("Server running");
 })
